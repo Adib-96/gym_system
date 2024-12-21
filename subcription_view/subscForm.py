c5e980751beaa0db7dd00bd303f4e55fd0098fc8
Adib-96 <adibbh1996@gmail.com>
2024-12-21 22:32:48 +0100

    add popup on success member creation

--- subcription_view/subscForm.py
+++ subcription_view/subscForm.py
@@ -1,15 +1,13 @@
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
+from kivy.uix.popup import Popup
 from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label  # Import the correct Label class
 from kivy.config import Config
 from kivy.core.window import Window
-from kivy.uix.togglebutton import ToggleButton
 from datetime import datetime
 
-
-#set configuration
+# Set configuration
 Window.clearcolor = (1, 1, 1, 1)  # RGBA: White
 
 Config.set('graphics', 'width', '1280')
@@ -20,6 +18,7 @@ Config.set('graphics', 'resizable', '1')
 class MainLayout(BoxLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.popup = None
 
     def submit_form(self):
         name = self.ids.name_input.text
@@ -27,66 +26,61 @@ class MainLayout(BoxLayout):
         email = self.ids.email_input.text
         activity = None
         sub_methode = None
-        ##iterate over Activities
-        for activ_button in [self.ids.bodybuilding, self.ids.karate,self.ids.aerobic]:
+
+        # Iterate over Activities
+        for activ_button in [self.ids.bodybuilding, self.ids.karate, self.ids.aerobic]:
             if activ_button.state == 'down':
                 activity = activ_button.text
                 break
 
-        ##iterate over sub methode
-
-        for sub_button in [self.ids.monthly,self.ids.s_20,self.ids.s_30]:
+        # Iterate over subscription methods
+        for sub_button in [self.ids.monthly, self.ids.s_20, self.ids.s_30]:
             if sub_button.state == 'down':
                 sub_methode = sub_button.text.split()[0]
                 break
-        print({'name':name, 'age':age, 'email':email,'date':datetime.now().strftime("%d/%m/%Y %H:%M:%S"), 'activity':activity, 'sub_methode':sub_methode})
-    pass
-"""
-    def on_button_press(self, instance):
 
-        ## activity logic to found the activity pressed
-        selected_activity = None
-        for button in [self.bodybuilding, self.karate, self.aerobic]:
-            if button.state == "down":
-                selected_activity = button.text
-                #to minimize algorithm loop :)
-                break
-
-
-        ## subscription methodes to found the one pressed
-        selected_sub_methode = None
-        for btn in [self.normal_sub, self.session_20, self.session_30, self.normal_sub]:
-            if btn.state == "down":
-                selected_sub_methode = btn.text
-                break
-
-
-        # Print input data on button press
         print({
-            "username": self.username.text,
-            "age": self.age.text,
-            "gym_activity": selected_activity,
-            "email": self.email.text,
-            "entry_time": MainLayout.get_current_time(),
-            "subscription_methode":selected_sub_methode
+            'name': name,
+            'age': age,
+            'email': email,
+            'date': datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
+            'activity': activity,
+            'sub_methode': sub_methode
         })
-        ## reset form fields
-        self.username.text = ""
-        self.age.text = ""
-        self.email.text = ""
-        button.state ="normal"
-        btn.state = "normal"
 
-        print(f"{instance.text} Button pressed!")
-    """
+        self.show_popup()
+
+        # Reset form fields
+        activ_button.state = 'normal'
+        sub_button.state = 'normal'
+        self.ids.name_input.text = ""
+        self.ids.age_input.text = ""
+        self.ids.email_input.text = ""
+
+    def show_popup(self):
+        layout = BoxLayout(orientation='vertical')
+        lbl = Label(text="Member created successfully",font_size="30pt",color=(0.4,0.5,0.6,1))  # Use Kivy's Label
+        close_button = Button(text="Close",size_hint=(0.4,0.1),pos_hint={'x':0.3})
+        close_button.bind(on_press=self.close_popup)  # Bind close button to dismiss the popup
+        layout.add_widget(lbl)
+        layout.add_widget(close_button)
+        self.popup = Popup(
+            title='Subscribe Form',
+            content=layout,
+            auto_dismiss=False,
+            size_hint=(0.8, 0.8),
+            size=(400, 400)
+        )
+        self.popup.open()
+
+    def close_popup(self, instance):
+        self.popup.dismiss()
 
 
-
-class SubForm(App):
+class HomeScreen(App):
     def build(self):
         return MainLayout()
 
 
-
 if __name__ == '__main__':
-    SubForm().run()
+    HomeScreen().run()
