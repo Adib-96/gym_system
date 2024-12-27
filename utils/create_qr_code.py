84a705c0df6e1017f4c7e9d7f64be666d71f98ba
Adib-96 <adibbh1996@gmail.com>
2024-12-27 21:23:07 +0100

    QR code generation with encypting and decrypting process

---
+++ utils/create_qr_code.py
@@ -0,0 +1,33 @@
+import os
+from dotenv import load_dotenv
+import qrcode
+import hmac
+import hashlib
+import base64
+
+# Load .env variables
+load_dotenv()
+key = os.getenv("QR_SECRET_KEY")
+
+
+def generate_qrcode(member_id):
+    data = f"{member_id}".encode('utf-8')
+    # Create HMAC object and hash the data
+    hmac_obj = hmac.new(key.encode('utf-8'), data, hashlib.sha512)  # Ensure key is encoded
+    hashed_data = hmac_obj.digest()
+
+    encoded_data = base64.urlsafe_b64encode(hashed_data).decode('utf-8')
+
+    qr = qrcode.QRCode(  # Corrected the typo here
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4
+    )
+
+    qr.add_data(encoded_data)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill="black", back_color="white")
+    img.save('../assets/{}.png'.format(member_id))  # Ensure valid file path and extension
+
