6eb6eab5b935166db5004cffc19d4a7365afcf39
Adib-96 <adibbh1996@gmail.com>
2025-01-14 06:58:08 +0100

    hot fix

--- gym_member_list/gymMembers.py
+++ gym_member_list/gymMembers.py
@@ -4,6 +4,7 @@ from kivy.uix.actionbar import Label
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import Screen
 from kivy.core.clipboard import Clipboard
+from kivy.uix.image import Image
 
 
 class GymMembers(BoxLayout):
@@ -12,9 +13,12 @@ class GymMembers(BoxLayout):
         super().__init__(**kwargs)
         
         #Todo fetch data from DB
-        self.users = []
+        self.users = [{},{},{}]
         
     def collect_filters(self):
+        
+        
+        
         username = self.ids.username_filter.text.strip()
         selected_activities = []
         if self.ids.bodybuilding.active:
@@ -33,22 +37,36 @@ class GymMembers(BoxLayout):
         if self.ids.session_30.active:
             selected_methods.append("30_session")
         
-        #!!!!!!!!!!!!!!!!!!!!!!!!
         
-        self.display_data(username=username,activity=selected_activities,subscription_method = selected_methods)
+        
+        #Todo===========================================================================================================
+        #Todo        FETCH DATA FROM WAREHOUSE THEN PASS THE RESULT  and passe it to SELF.USERS                         |
+        #Todo===========================================================================================================
+        
+        
+        #!!!!!!!!!!!!!!!!!!!!!!!!
+        #TodoDISPLAY_DATA(id,username,activity,subscription_methode,end_date,remaining_session)|
+        self.display_data()
         #!!!!!!!!!!!!!!!!!!!!!!!!
         
         
     def display_data(self,**kwargs):
-        # object destructring 
-        username,activity,subscription_method = kwargs
+        
+        member_id = kwargs.get('member_id')
+        username = kwargs.get('username')
+        activity = kwargs.get('activity')
+        subscription_method = kwargs.get('subscription_method')
+        end_date = kwargs.get('end_date')
+        remainig_session = kwargs.get('remaining_session')
+        
         
         ## clear old dataaaaa
         self.ids.user_display.clear_widgets()
         
         ##? here we gonna pull data from sqlite and push it down
         for user in self.users:
-            lb0 = Label(text=str(user['id']), size_hint_y=None, height=50,font_size=16)
+            
+            lb0 = Label(text="10", size_hint_y=None, height=50,font_size=16)
             lb0.bind(on_touch_down=self.copy_to_clipboard)
             with lb0.canvas.before:
                 Color(0.776, 0.675, 0.561, 1)
@@ -56,35 +74,35 @@ class GymMembers(BoxLayout):
             lb0.bind(size=self.update_rect, pos=self.update_rect)
             
             
-            lb1 = Label(text=user['username'], size_hint_y=None, height=50,font_size=16)
+            lb1 = Label(text="adib",size_hint_y=None, height=50,font_size=16)
             with lb1.canvas.before:
                 Color(0.776, 0.675, 0.561, 1)
                 Rectangle(size=lb1.size, pos=lb1.pos)
             lb1.bind(size=self.update_rect, pos=self.update_rect)
 
-            lb2 = Label(text=user['activity'], size_hint_y=None, height=50,font_size=16)
+            lb2 = Label(text="bodybulding",size_hint_y=None, height=50,font_size=16)
             with lb2.canvas.before:
                 Color(0.851, 0.851, 0.851, 1)
                 Rectangle(size=lb1.size, pos=lb1.pos)
             lb2.bind(size=self.update_rect, pos=self.update_rect)
 
-            lb3 = Label(text=user['subscription'], size_hint_y=None, height=50,font_size=16)
+            lb3 = Label(text="monthly" ,size_hint_y=None, height=50,font_size=16)
             with lb3.canvas.before:
                 Color(0.851, 0.851, 0.851, 1)
                 Rectangle(size=lb1.size, pos=lb1.pos)
             lb3.bind(size=self.update_rect, pos=self.update_rect)
 
-            lb4 = Label(text="ExpDate/RemainingSessions", size_hint_y=None, height=50,font_size=16)
-            with lb4.canvas.before:
+            img = Image(source="button.png" ,fit_mode="scale-down",size_hint= (None, None),size=(100,50))
+            with img.canvas.before:
                 Color(0.851, 0.851, 0.851, 1)
                 Rectangle(size=lb1.size, pos=lb1.pos)
-            lb4.bind(size=self.update_rect, pos=self.update_rect)
+            img.bind(size=self.update_rect, pos=self.update_rect)
             
             self.ids.user_display.add_widget(lb0)
             self.ids.user_display.add_widget(lb1)
             self.ids.user_display.add_widget(lb2)
             self.ids.user_display.add_widget(lb3)
-            self.ids.user_display.add_widget(lb4)
+            self.ids.user_display.add_widget(img)
     
     def update_rect(self, instance, value):
         instance.canvas.before.clear()
