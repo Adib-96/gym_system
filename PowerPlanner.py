b7bac4e86aadcc41bbff2dd83ffee4f39f03d8e0
Adib-96 <adibbh1996@gmail.com>
2025-01-19 07:27:18 +0100

    fix code and remove membership renewal from screen

--- PowerPlanner.py
+++ PowerPlanner.py
@@ -1,30 +1,34 @@
 from kivy.app import App
-from kivy.uix.screenmanager import ScreenManager,SlideTransition
+from kivy.uix.screenmanager import ScreenManager, SlideTransition
 from kivy.uix.dropdown import DropDown
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
+from kivy.lang.builder import Builder
+from kivy.graphics import Color, Rectangle
+
+# Import your screens
 from subcription_view.subscForm import HomeScreen
 from scan_view.scan_view import QRCodeScannerScreen
 from gym_member_list.gymMembers import MembersDisplay
-from kivy.lang.builder import Builder
-from membership_Renewal.membership_renewal import Renewal
 
+# Load KV files
 Builder.load_file("subcription_view/homescreen.kv")
 Builder.load_file("gym_member_list/gymmembers.kv")
-Builder.load_file("membership_Renewal/myapp.kv")
 
 class PowerPlanner(App):
     def build(self):
-
+        # ScreenManager setup
         self.sm = ScreenManager(transition=SlideTransition())
         self.sm.add_widget(HomeScreen(name="Membership Registration"))
         self.sm.add_widget(QRCodeScannerScreen(name="QR_code Screen"))
         self.sm.add_widget(MembersDisplay(name="MembersDisplay"))
-        self.sm.add_widget(Renewal(name="Membership Renewal"))
 
-
+        # Dropdown setup
         self.dropdown = DropDown()
-        screens = [("Membership Registration", "Membership Registration"), ("QR_code Screen", "QR_code Screen"), ("MembersDisplay", "MembersDisplay"),("Membership Renewal","Membership Renewal")]
+        screens = [("Membership Registration", "Membership Registration"), 
+                   ("QR_code Screen", "QR_code Screen"), 
+                   ("MembersDisplay", "MembersDisplay"),
+                   ]
 
         self.selected_button = None
 
@@ -37,29 +41,32 @@ class PowerPlanner(App):
         main_button = Button(text="Select Screen", size_hint=(None, None), size=(180, 44))
         main_button.bind(on_release=self.dropdown.open)
 
-
+        # BoxLayout setup with background color
         layout = BoxLayout(orientation="vertical")
+        with layout.canvas.before:
+            Color(0,0,0,0.7)  # Light grey background
+            self.rect = Rectangle(size=layout.size, pos=layout.pos)
+        layout.bind(size=self._update_rect, pos=self._update_rect)
+        layout.padding = 20
         layout.add_widget(main_button)
         layout.add_widget(self.sm)
 
         return layout
 
+    def _update_rect(self, instance, value):
+        self.rect.size = instance.size
+        self.rect.pos = instance.pos
+
     def on_button_click(self, button, screen_name):
-        # Reset background of the previously selected button
         if self.selected_button:
             self.selected_button.background_color = (1, 1, 1, 1)
-
-        # Update the current button's background
         button.background_color = (0.5, 1, 0.5, 1)
         self.selected_button = button
-
-
         self.switch_screen(screen_name)
 
     def switch_screen(self, screen_name):
         self.sm.current = screen_name
         self.dropdown.dismiss()
-
 
 if __name__ == "__main__":
     PowerPlanner().run()
